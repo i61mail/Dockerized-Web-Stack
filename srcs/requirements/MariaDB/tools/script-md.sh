12714fc1f081c14104354213ec097d23d242e4f2
i61mail <ismailrk55@gmail.com>
2025-09-09 21:48:17 +0000

    fix mariadb not starting

--- srcs/requirements/MariaDB/tools/script-md.sh
+++ srcs/requirements/MariaDB/tools/script-md.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+mysqld_safe --datadir='/var/lib/mysql' &
+MYSQL_PID=$!
 
+until mysqladmin ping -u root -p"$ROOT_PASSWORD" --silent; do
+    sleep 1
+done
 
-exec mysqld_safe
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DATA_BASE}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS '${USER}'@'%' IDENTIFIED BY '${PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${DATA_BASE}\`.* TO '${USER}'@'%';"
+mariadb -e "FLUSH PRIVILEGES;"
+
+kill $MYSQL_PID
+wait $MYSQL_PID 2>/dev/null || true 
+
+exec mysqld_safe --datadir='/var/lib/mysql' --bind-address=0.0.0.0
